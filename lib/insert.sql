740844b89733b44b1360558409a52bdab8a2bc2c
Max Zakzook <mzakzook@me.com>
2019-09-24 22:35:45 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 20, "Male", "Chocolate", "Sweet", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 6, "Female", "Red", "Angry", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 28, "Female", "Purple", "Confounded", false);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 89, "Male", "Grey", "Sour", false);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 19, "Female", "Pink", "Kind", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 22, "Male", "Green", "Mischievous", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 13, "Female", "Turquoise", "Wicked", true);
+
+INSERT INTO bears (age, gender, color, temperament, alive)
+VALUES (30, "Female", "Spotted", "Absent", true);
