cbad1a7d566add64f7666fc213aa083193e11ebb
Urfan Guliyev <urfan196@yahoo.com>
2019-09-26 02:55:05 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+-- INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+-- ("Mr. Chocolate", 2, "female", "brown", "mad", 0),
+-- ("Rowdy", 10, "male", "white", "mad", 1),
+-- ("Tabitha", 3, "male", "black", "mad", 1),
+-- ("Sergeant Brown", 3, "male", "brown", "mad", 0),
+-- ("Melissa", 5, "female", "black", "goofy", 1),
+-- ("Grinch", 2, "male", "brown", "mad", 0),
+-- ("Wendy", 1, "female", "black", "happy", 1)
+-- (null, 3, "male", "brown", "mad", 0);
+
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "female", "brown", "mad", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "male", "brown", "mad", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "male", "black", "mad", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 3, "male", "brown", "mad", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "female", "black", "goofy", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "male", "brown", "mad", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 1, "female", "black", "happy", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 3, "male", "brown", "mad", true);
